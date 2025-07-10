e14d14dda9b3c07c02c737cb122e8fff43d3c789
isaqxd <isaque3356@icloud.com>
2025-07-10 09:56:45 -0300

    Criando entidade Agencia

---
+++ bank/src/main/java/calisto/bank/entity/Endereco/Endereco.java
@@ -0,0 +1,19 @@
+package calisto.bank.entity.Endereco;
+
+import calisto.bank.entity.Usuario.Usuario;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Endereco {
+	private int idEndereco;
+	private Usuario usuario;
+	private String cep;
+	private String local;
+	private int numeroCasa;
+	private String bairro;
+	private String cidade;
+	private char estado;
+	private String complemento;
+}
